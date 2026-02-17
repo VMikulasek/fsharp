44f7def8b6498630c11c9c407786114a716d4ec9
VMikulasek <mikulasekvitek@gmail.com>
2026-02-17 22:44:51 +0100

    Symbols: anonymous type-tagged union hashing

--- src/Compiler/Symbols/Symbols.fs
+++ src/Compiler/Symbols/Symbols.fs
@@ -2791,8 +2791,7 @@ type FSharpType(cenv, ty:TType) =
             | TType_fun (domainTy, rangeTy, _) -> 10500 + hashType domainTy + hashType rangeTy
             | TType_measure _ -> 10600 
             | TType_anon (_,l1) -> 10800 + List.sumBy hashType l1
-            // TODO: Anonymous type-tagged union
-            | TType_anon_tt_union (_, _) -> failwith "Anonymous type-tagged unions not implemented yet"
+            | TType_anon_tt_union (_, l1) -> 10900 + List.sumBy hashType l1
         hashType ty
 
     member _.Format(context: FSharpDisplayContext) = 
