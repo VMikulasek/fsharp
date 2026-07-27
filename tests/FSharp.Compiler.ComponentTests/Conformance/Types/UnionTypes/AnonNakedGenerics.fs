8b8d59781c4b758a64ff457e4cb78a4da011185d
VMikulasek <mikulasekvitek@gmail.com>
2026-07-27 11:14:16 +0200

    anonymous unions: constraint solving
    - fix constraint solving for type :> anonymous union (add postponement
      and cs errors for no/ambiguous solution)
    - add error test for ambiguous solution

--- tests/FSharp.Compiler.ComponentTests/Conformance/Types/UnionTypes/AnonNakedGenerics.fs
+++ tests/FSharp.Compiler.ComponentTests/Conformance/Types/UnionTypes/AnonNakedGenerics.fs
@@ -6,5 +6,5 @@ let decide<'T, 'U>(x: ('T | 'U)): int =
     | :? 'T -> 0
     | :? 'U -> 1
 
-if not (decide<int, string>(42) = 0) then failwith "Test failed"
-if not (decide<int, string>("asd") = 1) then failwith "Test failed"
+if not (decide<int, string> 42 = 0) then failwith "Test failed"
+if not (decide<int, string> "asd" = 1) then failwith "Test failed"
